a80ed8a88fa91776cd6ed28a3644e5485c1e447b
Tito Miguel <tito.javier.miguel.galdamez@gmail.com>
2016-01-21 12:05:32 -0600

    :calling: Instalar con comandos una instancia de SQL Server desde un archivo de configuración.

---
+++ SQLServer/instalacion/Instalacion con archivo.bat
@@ -0,0 +1,5 @@
+::*****************************
+::*Ejecutar como Administrador
+::*****************************
+
+F:\setup.exe /CONFIGURATIONFILE="ConfigurationFile.ini" /SAPWD="12345" /SQLSVCPASSWORD="12345" /AGTSVCPASSWORD="12345" /ISSVCPASSWORD="12345" /ASSVCPASSWORD="12345" /RSSVCPASSWORD="12345" 
